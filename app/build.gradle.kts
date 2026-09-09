2b608f0d2e1e9da303a548daf7f7539ca7a1b9bc
E7G <55611737+E7G@users.noreply.github.com>
2026-09-09 08:49:23 +0800

    chore: release 1.10.5 asr isolated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,8 +87,8 @@ android {
         minSdk = 26
         targetSdk = 35
         ndk { abiFilters += listOf("arm64-v8a") }
-        versionCode = 204
-        versionName = "1.10.4-stability"
+        versionCode = 205
+        versionName = "1.10.5-asr-isolated"
         vectorDrawables.useSupportLibrary = true
     }
 
